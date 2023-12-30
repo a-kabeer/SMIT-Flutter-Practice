fee13181ed2ffb1a4e6332ccd0da889fc8ab8ff5
a-kabeer <a.kabeer1991@gmail.com>
2023-12-30 19:07:35 +0500

    Assignment 01 - Abdul Kabeer - 181902

--- Assignment 01/Q7-squareRoot.dart
+++ Assignment 01/Q7-squareRoot.dart
@@ -1,13 +1,13 @@
 import 'dart:math';
 
-void main() {
+void main() { 
   //Q7: Write a program to calculate root of any number. i.e: √y = y1⁄2
-//Method 1
+  ////Method 1
   int y = 16;
   num sqaureRoot = pow(y, 0.5);
   print("Squareroot of $y is $sqaureRoot");
-
-//Method 2
+  
+  //Method 2
   int x = 9;
   num squareRoot1 = sqrt(x);
   print("Squareroot of $x is $squareRoot1");
