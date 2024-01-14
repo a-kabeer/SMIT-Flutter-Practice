dfcc0f87d03a00cdfaecf29ac264a23582728d2f
a-kabeer <a.kabeer1991@gmail.com>
2024-01-14 15:50:27 +0500

    Practice

--- Practice-01/forloop.dart
+++ Practice-01/forloop.dart
@@ -1,9 +1,18 @@
 import 'dart:io';
 
 void main() {
+  //Table
   print("Enter a Number to Generate Table");
   int num1= int.parse(stdin.readLineSync()!);
-  for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 10; i++)
     print("$num1 X $i= ${num1 * i}");
+  
+  // List Data
+  List<String> studentName= ["Abu Bakr","Umar","Usman","Ali","Talha","Zubair","Abdur Rehman","Bilal","Abdul Kabeer","Adeel","Zahid"];
+   studentName.sort();
+   var a =studentName.reversed;
+      for (int i = 0; i <= studentName.length-1; i++) {
+    print(a.toList()[i]);
+  
   }
-}
+  }
