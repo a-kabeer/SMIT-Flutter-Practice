e0db59df4a0be9562874e29daaecfe321973a17f
a-kabeer <a.kabeer1991@gmail.com>
2024-01-17 20:17:07 +0500

    Practice

---
+++ Practice-01/forpractice.dart
@@ -0,0 +1,20 @@
+void main() {
+  List studentname = [
+    "ali",
+    "zaid",
+    "ahmed",
+    "muaviya",
+    "ameer hamza",
+    "kabeer",
+    "sameer",
+    "sattar",
+    "sumair",
+    "huzaifa",
+    "khalid",
+    "waleed",
+    "amar"
+  ];
+  for (int i = 0; i < studentname.length; i = i + 1) {
+    print(studentname[i]);
+  }
+}
---
+++ Practice-01/forrlopp.dart
@@ -0,0 +1,6 @@
+void main (){
+  
+  for(int i = -3; i<2; i=i+1){
+    print("huzaifa $i");
+  }
+}
