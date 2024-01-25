74b22cb7e043e298d2cd72eddd16a574760416dc
a-kabeer <a.kabeer1991@gmail.com>
2024-01-25 14:32:52 +0500

    Assignment-02

---
+++ Assignment-02/getSmallestandGreatest.dart
@@ -0,0 +1,8 @@
+void main() {
+  //Q.4: Create a list of numbers & write a program to get the smallest & greatest number from a list.
+  List<int> numbers = [2, 3, 1, 5, 7, 43, 22, 56, 33, 68, 89, 2, 12, 5, 56];
+  print(numbers);
+  numbers.sort();
+  print(numbers);
+  print("Smallest Number= ${numbers[0]}\nGreatest Number= ${numbers.last}");
+}
