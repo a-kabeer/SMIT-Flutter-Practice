78dbc7f7fb2a8f6edd8322537c2c07cd8b8a0448
a-kabeer <a.kabeer1991@gmail.com>
2024-01-25 15:47:01 +0500

    Assignment-02

--- Assignment-02/Q7-updateMap.dart
+++ Assignment-02/Q7-updateMap.dart
@@ -1,11 +1,5 @@
 void main() {
-//   Map<String, double> expenses = {
-// 'sun': 3000.0,
-// 'mon': 3000.0,
-// 'tue': 3234.0,
-// };
-// Check if "fri" exist in expanses; if exist change it's value to 5000.0 otherwise
-// add 'fri' to expenses and set its value to 5000.0 then print expenses.
+// Check if "fri" exist in expanses; if exist change it's value to 5000.0 otherwise add 'fri' to expenses and set its value to 5000.0 then print expenses.
   Map<String, double> expenses = {
     'sun': 3000.0,
     'mon': 3000.0,
