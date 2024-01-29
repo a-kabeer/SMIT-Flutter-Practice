d6abbcb851a0dbbda23b68350f2b5acc03750788
a-kabeer <a.kabeer1991@gmail.com>
2024-01-29 15:32:37 +0500

    Practice

--- Practice/pyramid.dart
+++ Practice/pyramid.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
 void main() {
-  var row = 4;
+  var row = 5;
+  num counter = 0;
   var space = " ";
   for (var i = 1; i < row; i++) {
     stdout.write("${space * (row - i)}");
     for (var j = 1; j <= i; j++) {
-      stdout.write("*$space");
+      stdout.write("${++counter}$space");
     }
     print("");
   }
