ec6bfc413ba56d179e0a21ded13697d0fd16e526
a-kabeer <a.kabeer1991@gmail.com>
2024-02-03 19:41:33 +0500

    Assignment-03

---
+++ Assignment-03/Q14-.spaceDifferentNumber.dart
@@ -0,0 +1,17 @@
+//Write a program to make such a pattern like a right angle triangle with the number increased by 1 using loop..
+//The pattern like :
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+import 'dart:io';
+
+void main() {
+  int count = 1;
+  for (var i = 0; i < 5; i++) {
+    for (var j = 1; i >= j; j++) {
+      stdout.write("${count++} ");
+    }
+    print("");
+  }
+}
